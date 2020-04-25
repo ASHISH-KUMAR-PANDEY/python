4a22f6681e89f8720b13dddbb51eea43c24e5c76
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-04-26 03:22:00 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,29 @@
+
+# Python3 code to count vowel in
+# a string using set
+
+# Function to count vowel
+def vowel_count(str):
+
+    # Initializing count variable to 0
+    count = 0
+
+    # Creating a set of vowels
+    vowel = set("aeiouAEIOU")
+
+    # Loop to traverse the alphabet
+    # in the given string
+    for alphabet in str:
+
+        # If alphabet is present
+        # in set vowel
+        if alphabet in vowel:
+            count = count + 1
+
+    print("No. of vowels :", count)
+
+# Driver code
+str = "aqwertyuiopasdfghjklzxcvbnm"
+
+# Function Call
+vowel_count(str)
