604e635dcedd6e3c6fddbe794338f6e7236fbb07
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-05-06 23:54:57 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,5 +2,7 @@ import string
 import random
 
 def randompassword():
-
+    chars=string.ascii_uppercase + string.ascii_lowercase + string.digits
+    size=8 
+    return ''.join(random.choice(chars) for x in range(size,12))
 print(randompassword())
