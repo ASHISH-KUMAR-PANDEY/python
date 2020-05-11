9c82c4d9bb9951f1f55a297f4f724e418ce8d871
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-05-12 00:24:58 +0530

    Update circumsized_circle.py

--- circumsized_circle.py
+++ circumsized_circle.py
@@ -1,3 +1,5 @@
 def square_areas_difference(r):
     length_big_square = 2 * r
     area_big_square = (2 * r)  ** 2
+    area_small_square = r**2 + r**2
+    return area_big_square - area_small_square
