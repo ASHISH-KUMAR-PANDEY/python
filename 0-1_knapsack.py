2984d9876daf2225125bbe53ed5b4d41515290e1
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-05-19 20:57:19 +0530

    Update 0-1_knapsack.py

--- 0-1_knapsack.py
+++ 0-1_knapsack.py
@@ -1,19 +1,6 @@
-# A naive recursive implementation 
-# of 0-1 Knapsack Problem 
-  
-# Returns the maximum value that  
-# can be put in a knapsack of  
-# capacity W 
 def knapSack(W, wt, val, n): 
-  
-    # Base Case 
     if n == 0 or W == 0 : 
         return 0
-  
-    # If weight of the nth item is  
-    # more than Knapsack of capacity W,  
-    # then this item cannot be included  
-    # in the optimal solution 
     if (wt[n-1] > W): 
         return knapSack(W, wt, val, n-1) 
   
