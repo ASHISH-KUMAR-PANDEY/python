53283b03d7cbd5c536d89b81d1aeeb116aeef784
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-05-23 00:29:20 +0530

    Update give_me_a_hint.py

--- give_me_a_hint.py
+++ give_me_a_hint.py
@@ -6,4 +6,17 @@ def grant_the_hint(txt):
         l = len(i)
         row = ['_'*l]
         ind = 1
-   
+        for j in range(len(i)):
+            el = i[:ind] + ('_'*(l-ind))
+            row.append(el)
+            ind+=1
+        for j in range(m-l):
+            row.append(i)
+        ans.append(row)    
+    a = []
+    for i in range(len(ans[0])):
+        bl = []
+        for j in range(len(ans)):
+            bl.append(ans[j][i])
+        a.append(" ".join(bl))  
+    return a
