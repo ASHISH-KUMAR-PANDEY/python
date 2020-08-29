6fcfe07c43163073851b789e9c45a41d839a7336
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-08-29 22:23:16 +0530

    Create connected_graph.py

---
+++ connected_graph.py
@@ -0,0 +1,13 @@
+def is_connected(g):
+	start_node = list(g.keys())[0]
+	color = {v: 'white' for v in g.keys()}
+	color[start_node] = 'gray'
+	S = [start_node]
+	while len(S) != 0:
+		u = S.pop()
+		for v in g[u]:
+			if color[v] == 'white':
+				color[v] = 'gray'
+				S.append(v)
+			color[u] = 'black'
+	return list(color.values()).count('black') == len(g.keys())
